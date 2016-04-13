9c56f702cd9f51ee44c3326e4ee49e1ce64969d1
Jay Kan <leicasper@gmail.com>
2016-04-13 16:26:36 -0700

    feat(dropdown-toggle): add dropdown-toggle component

--- src/components/dropdown/dropdown-toggle.directive.ts
+++ src/components/dropdown/dropdown-toggle.directive.ts
@@ -1,3 +1,34 @@
-export class DropdownToggle {
+import { Directive, ElementRef, Host, OnInit, Input, HostBinding, HostListener } from 'angular2/core';
+import { Dropdown } from './dropdown.directive';
+
+@Directive({ selector: '[dropdownToggle]' })
+export class DropdownToggle implements OnInit {
+  @HostBinding('class.disabled')
+  @Input() private disabled: boolean = false;
+  
+  @HostBinding('class.dropdown-toggle')
+  @HostBinding('attr.aria-haspopup')
+  private addClass = true;
+  
+  constructor(@Host() public dropdown: Dropdown, public el: ElementRef) {}
+  
+  ngOnInit(): void {
+    this.dropdown.dropDownToggle = this;
+  }
+  
+  @HostBinding('attr.aria-expanded')
+  public get isOpen(){
+    return this.dropdown.isOpen;
+  }
+  
+  @HostListener('click', ['$event'])
+  public toggleDropdown(event: MouseEvent): boolean {
+    event.stopPropagation();
+    
+    if (!this.disabled) {
+      this.dropdown.toggle();  
+    }
+    return false;
+  }
   
 }
