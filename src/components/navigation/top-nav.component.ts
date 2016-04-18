2847a08ccbc38e367633c21ceb340a0f14354be7
Jay Kan <leicasper@gmail.com>
2016-04-18 12:25:22 -0700

    refactor(top-nav): integrate custom dropdown dropdownToggle components

--- src/components/navigation/top-nav.component.ts
+++ src/components/navigation/top-nav.component.ts
@@ -1,11 +1,12 @@
 import { Component, ViewEncapsulation } from 'angular2/core';
 import { Router } from 'angular2/router';
 import { CORE_DIRECTIVES } from 'angular2/common';
+import { DROPDOWN_DIRECTIVES } from '../dropdown/dropdown';
 
 @Component({
   selector: 'top-nav',
   templateUrl: 'components/navigation/top-nav.component.html',
-  directives: [CORE_DIRECTIVES],
+  directives: [CORE_DIRECTIVES, DROPDOWN_DIRECTIVES],
   encapsulation: ViewEncapsulation.None,
   styles: [`
     .topnav-navbar {
@@ -38,6 +39,27 @@ import { CORE_DIRECTIVES } from 'angular2/common';
     ul.nav.navbar-nav .nav-item {
       float: left;
     }
+    .open > .dropdown-menu {
+        display: block;
+    }
+    .open > a {
+        outline: 0;
+    }
+    .float-right {
+        float: right;
+    }
+    .navbar-nav .open .dropdown-menu {
+      position: absolute;           
+       background-color: #fff;
+      -webkit-background-clip: padding-box;
+      background-clip: padding-box;
+      border: 1px solid #ccc;      
+      margin-top: 2px;
+      border-radius: 0;
+      -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
+      box-shadow: 0 6px 12px rgba(0,0,0,.175);      
+    }
+   
     @media screen and (max-width: 768px) {
       .theme-name {
         position: absolute;
@@ -67,29 +89,44 @@ import { CORE_DIRECTIVES } from 'angular2/common';
       .topnav-navbar #sidebar-toggler:focus {
         outline: none; 
       }
-      .hidd .dropdown a img {
+      
+      .navbar-nav.hidd .dropdown a img {
         cursor: pointer;
         width: 35px;
         margin: 8px 15px 0 0;
       }
 
-      .hidd .dropdown a:after {
+      .navbar-nav.hidd .dropdown a:after {
           display: none;
       }
   
-      .hidd .dropdown .dropdown-menu {
+      .navbar-nav.hidd .dropdown .dropdown-menu {
           left: auto;
           right: 0;
           top: 48px;
       }
   
-      .hidd .dropdown .dropdown-menu a {
+      .navbar-nav.hidd .dropdown .dropdown-menu a {
           margin: -8px 0;
           padding: 10px 15px;
       }
+      .navbar-nav.hidd .dropdown .dropdown-item {
+          display: block;
+          width: 100%;
+          padding: 3px 20px;
+          clear: both;
+          font-weight: normal;
+          line-height: 1.5;
+          color: #373a3c;
+          text-align: inherit;
+          white-space: nowrap;
+          background: none;
+          border: 0;
+      }
   
-      .hidd .dropdown .dropdown-menu a:hover {
+      .navbar-nav.hidd .dropdown .dropdown-menu a:hover {
           background: #2c3e50;
+          color: #fff;
       }
      
       img.profile-avatar {
@@ -98,13 +135,13 @@ import { CORE_DIRECTIVES } from 'angular2/common';
         width: 35px;
         margin: 8px 15px 0 0;
       }
+      
     }
   `]
 })
 
 export class TopNavComponent {
 
-  constructor(private _router: Router) {
-  }
+  constructor(private _router: Router) {}
   
 }
