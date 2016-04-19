f4e1e3889f268531ba2af3c8f9fec615c12c3142
Jay Kan <leicasper@gmail.com>
2016-04-19 12:24:13 -0700

    refactor(home): integrate StatsWidget components

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,43 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit, ViewEncapsulation } from 'angular2/core';
 import { StatsWidget } from '../../components/stats/stats';
 
 @Component({
   selector: 'home',
   template: `
-  <h1>This is Dashboard Home View</h1>
+  <h4>Dashboard@Home</h4>
   <div class="animate">
     <div class="row home-row">    
       <div class="col-lg-4 col-xl-3">
-        <div></div>
+        <div class="home-stats">          
+          <div *ngFor="#record of stats">
+            <stats-widget
+              [icon]="record.icon"
+              [value]="record.value"
+              [text]="record.text"
+              [bgClass]="record.bgClass"
+              [progressValue]="record.progressValue"
+            >            
+            </stats-widget>
+          </div>                   
+        </div>
       </div>
     </div>
   </div>   
-  `  
+  `,
+  directives: [StatsWidget],
+  encapsulation: ViewEncapsulation.None
 })
 
-export class Home {
-  
+export class Home implements OnInit {
+  stats: Array<any>;
+
+  ngOnInit(): void {
+    this.stats = [
+      { icon: 'cloud-upload', value: '88%', text: 'Server Uptime', bgClass: 'info', progressValue: '88' },
+      { icon: 'heartbeat', value: '94%', text: 'Positive feedback', bgClass: 'success', progressValue: '94' },
+      { icon: 'flag', value: '12,351', text: 'Posts flagged', bgClass: 'danger', progressValue: '72' },
+    ]
+  }
+
+
 }
