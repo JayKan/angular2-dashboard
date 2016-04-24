1943408d91b6bf62c5e121716263b0fb8e6beb71
Jay Kan <leicasper@gmail.com>
2016-04-24 13:53:50 -0700

    feat(user-profile): add user-profile-widget component

--- src/components/user/profile.component.ts
+++ src/components/user/profile.component.ts
@@ -10,17 +10,17 @@ import { Component, ViewEncapsulation } from 'angular2/core';
   `,
   encapsulation: ViewEncapsulation.None,
   styles: [`
-.profile .avatar-name {
-    padding-top: 12px;
-    font-size: 14px;
-}
-.profile .profile-icon {
-    border-radius: 50%;
-    width: 125px;
-}
-.profile a {
-     cursor: pointer;
-}
+  .profile .avatar-name {
+      padding-top: 12px;
+      font-size: 14px;
+  }
+  .profile .profile-icon {
+      border-radius: 50%;
+      width: 125px;
+  }
+  .profile a {
+       cursor: pointer;
+  }
   `]
 })
 export class UserProfileWidget {
