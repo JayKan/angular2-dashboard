856879505f6d027627b9d71c1e9357245000cfad
Jay Kan <leicasper@gmail.com>
2016-04-26 11:23:07 -0700

    refactor(tables-view): add sample tables

--- src/pages/tables/tables.ts
+++ src/pages/tables/tables.ts
@@ -5,9 +5,48 @@ import { Component, OnInit } from 'angular2/core';
   template:`
   <h4>Dashboard@Tables</h4>
   <div class="animate">
-    <h3>Tables Content</h3>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header card-primary card-inverse">
+            <span class="card-title">Regular Table</span>
+          </div>
+          <div class="card-block">
+            <table class="table">
+              <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>John</td>
+                  <td>john@gmail.com</td>
+                  <td>London, UK</td>
+                </tr>
+                <tr>
+                  <td>Andy</td>
+                  <td>andygmail.com</td>
+                  <td>Merseyside, UK</td>
+                </tr>
+                <tr>
+                  <td>Frank</td>
+                  <td>frank@gmail.com</td>
+                  <td>Southampton, UK</td>
+                </tr>
+              </tbody>
+            </table>            
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  `
+  `,
+  styles: [`
+  
+  `]
 })
 export class TablesView implements OnInit {
 
