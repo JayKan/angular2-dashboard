ecdd685177a7455e4633547465481e96bc0488df
Jay Kan <leicasper@gmail.com>
2016-05-12 11:55:11 -0700

    refactor(grid-view): add styleURL

--- src/pages/grids/grids.ts
+++ src/pages/grids/grids.ts
@@ -4,25 +4,6 @@ import { Component, ViewEncapsulation } from 'angular2/core';
   selector: 'grids',
   encapsulation: ViewEncapsulation.None,
   templateUrl: 'pages/grids/grids.template.html',
-  styles: [`
-  .grid {
-    position: relative;
-    display: block;
-    margin-bottom: 0.625rem;
-    background-color: #fff;
-    border: 1px solid #e5e5e5;
-  }
-  .grid .grid-block {
-    padding: 16px 0;
-  }
-  .center {
-    text-align: center;
-  }
-  .grid .grid-block h6 {
-    margin: 10px 0;
-    font-size: 14px;
-    font-weight: 400;
-  }
-  `]
+  styleUrls: ['pages/grids/grids.style.css']
 })
 export class GridsView {}
