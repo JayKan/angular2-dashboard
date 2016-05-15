098c6e24fc0fe60247dda7e005cff604f63e6f91
Jay Kan <leicasper@gmail.com>
2016-05-15 12:25:05 -0700

    refactor(user-profile-widget): add styleUrl

--- src/components/user/profile.component.ts
+++ src/components/user/profile.component.ts
@@ -9,19 +9,7 @@ import { Component, ViewEncapsulation } from 'angular2/core';
   </center>
   `,
   encapsulation: ViewEncapsulation.None,
-  styles: [`
-  .profile .avatar-name {
-      padding-top: 12px;
-      font-size: 14px;
-  }
-  .profile .profile-icon {
-      border-radius: 50%;
-      width: 125px;
-  }
-  .profile a {
-       cursor: pointer;
-  }
-  `]
+  styleUrls: ['components/user/profile.style.css']
 })
 export class UserProfileWidget {
   
