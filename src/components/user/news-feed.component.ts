2eb1b6f505ce4b373e560fe125e7831e9b96bed5
Jay Kan <leicasper@gmail.com>
2016-06-07 12:51:44 -0700

    refactor(news-feed): add templateUrl and fix styleUrls

--- src/components/user/news-feed.component.ts
+++ src/components/user/news-feed.component.ts
@@ -2,28 +2,8 @@ import { Component, ViewEncapsulation } from 'angular2/core';
 
 @Component({
   selector: 'news-feed-widget',
-  template:`
-  <div id="news-feed">
-    <div class="feed-header">NEWS FEEDS</div>
-    <div class="feed-content">
-      <ul class="feed">
-        <li>
-          <a>Conference on the sales results for the previous year</a> <span class="feed-date">25/4/2015</span>
-        </li>
-        <li>
-          <a>Jay's birthday at San Pedro Square at 7:00pm.</a> <span class="feed-date">25/4/2015</span>
-        </li>
-        <li>
-          <a>Kobe bryant commented on your post.</a> <span class="feed-date">25/4/2015</span>
-        </li>
-        <li>
-          <a>Password Alert: Need to upgrade your password.</a> <span class="feed-date">25/4/2015</span>
-        </li>
-      </ul>
-    </div>
-  </div>
-  `,
+  templateUrl: 'components/user/news-feed.template.html',
   encapsulation: ViewEncapsulation.None,
-  stylesUrls: ['components/user/news-feed.style.css']
+  styleUrls: ['components/user/news-feed.style.css']
 })
 export class NewsFeedWidget {}
