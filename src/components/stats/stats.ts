02395688470a422c2d05ec1f8ace9557a97a9176
Jay Kan <leicasper@gmail.com>
2016-06-07 09:21:54 -0700

    refactor(stats): add temlateUrl

--- src/components/stats/stats.ts
+++ src/components/stats/stats.ts
@@ -2,24 +2,7 @@ import { Component, Input, ViewEncapsulation } from 'angular2/core';
 
 @Component({
   selector: 'stats-widget',
-  template: `
-  <a class="stat card hvr-wobble-horizontal">
-    <div class="stat-icon">
-      <i class="fa fa-{{ icon }} fa-4x text-{{ bgClass }}"></i>
-    </div>
-    <div class="stat-label">
-      <div class="label-header">
-        {{ value }}
-      </div>
-      <progress class="progress progress-{{ bgClass }}" max="100" value="{{ progressValue }}">{{ progressValue }}%</progress>
-      <div class="clearfix stat-detail">
-        <div class="label-body">
-          <i class="fa fa-arrow-circle-o-right pull-right text-muted"></i> {{ text }}
-        </div>
-      </div>
-    </div>
-  </a>    
-  `,
+  templateUrl: 'components/stats/stats.template.html',
   styleUrls: ['components/stats/stats.style.css'],
   encapsulation: ViewEncapsulation.None
 })
