556fac6306a87fa80382430ee17aa6d8567e7bb5
Jay Kan <leicasper@gmail.com>
2016-06-08 15:21:43 -0700

    chore(): add systemjs config

---
+++ src/system.config.js
@@ -0,0 +1,29 @@
+;(function() {
+  'use strict';
+  
+  // map tells the System loader where to look for things
+  var map = {
+    'rxjs':       'node_modules/rxjs',
+    '@angular':   'node_modules/@angular',
+    "@angular/core": "node_modules/@angular/core/index.js",
+    "@angular/common": "node_modules/@angular/common/index.js",
+    "@angular/compiler": "node_modules/@angular/compiler/index.js",
+    "@angular/platform-browser": "node_modules/@angular/platform-browser/index.js",
+    "@angular/platform-browser-dynamic": "node_modules/@angular/platform-browser-dynamic/index.js",
+    "@angular/router-deprecated": "node_modules/@angular/router-deprecated/index.js",
+    "@angular/router": "node_modules/@angular/router/@angular/index.js"
+  };
+
+  var config = {
+    defaultJSExtensions: true,
+    map: map,
+    packages: {
+      'rxjs': {
+        defaultExtension: 'js'
+      }
+    }
+  };
+
+  System.config(config);
+  
+})();
