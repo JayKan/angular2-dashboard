895ecbdd7e6f91003ec133c3653c53298e88c057
Jay Kan <leicasper@gmail.com>
2016-06-13 09:58:52 -0700

    refactor(charts): add templateUrl

--- src/pages/charts/charts.ts
+++ src/pages/charts/charts.ts
@@ -11,46 +11,7 @@ import {
 
 @Component({
   selector: 'charts',
-  template:`
-  <h4>Dashboard@Charts</h4>
-  <div class="animate">
-    <div class="row">
-      <div class="col-md-6">
-        <bubble-chart-widget></bubble-chart-widget>    
-      </div>
-      
-      <div class="col-md-6">
-        <line-chart-cmp></line-chart-cmp>
-      </div>
-    </div>
-    
-    <div class="row">
-      <div class="col-md-6">
-        <pie-chart-cmp></pie-chart-cmp>
-      </div>      
-      <div class="col-md-6">
-        <polar-area-chart-widget></polar-area-chart-widget>
-      </div>
-    </div>
-    
-    <div class="row">
-      <div class="col-md-6">
-        <combo-line-bar-widget></combo-line-bar-widget>
-      </div>
-      
-      <div class="col-md-6">
-        <radar-chart-widget></radar-chart-widget>
-      </div>
-    </div>
-    
-    <div class="row">
-      <div class="col-md-6">
-        <scatter-chart-widget></scatter-chart-widget>
-      </div>
-    </div>
-  </div>
-   
-  `,
+  templateUrl: 'pages/charts/charts.template.html',
   directives: [
     BubbleChartWidget,
     LineChartComponent,
