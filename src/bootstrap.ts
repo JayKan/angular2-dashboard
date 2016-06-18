50c575a78edfe94a420030e4ff6551c4da36268e
Jay Kan <leicasper@gmail.com>
2016-06-18 13:50:39 -0700

    chore: clean up

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,8 +1,11 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { DemoApp } from './demo-app/demo-app';
 import { ROUTER_PROVIDERS } from '@angular/router';
+import { HTTP_PROVIDERS } from '@angular/http';
 
-bootstrap(DemoApp, [ROUTER_PROVIDERS])
-  .then(success => console.log('Bootstrap success'))
-  .catch(error => console.error(error))
-;
+bootstrap(DemoApp, [
+  ROUTER_PROVIDERS,
+  HTTP_PROVIDERS
+])
+.then(success => console.log('Bootstrap success'))
+.catch(error => console.error(error));
--- src/system.config.js
+++ src/system.config.js
@@ -3,15 +3,16 @@
   
   // map tells the System loader where to look for things
   var map = {
-    'rxjs':       'node_modules/rxjs',
-    '@angular':   'node_modules/@angular',
-    "@angular/core": "node_modules/@angular/core/index.js",
-    "@angular/common": "node_modules/@angular/common/index.js",
-    "@angular/compiler": "node_modules/@angular/compiler/index.js",
-    "@angular/platform-browser": "node_modules/@angular/platform-browser/index.js",
-    "@angular/platform-browser-dynamic": "node_modules/@angular/platform-browser-dynamic/index.js",
-    "@angular/router-deprecated": "node_modules/@angular/router-deprecated/index.js",
-    "@angular/router": "node_modules/@angular/router/index.js"
+    'rxjs':                               "node_modules/rxjs",
+    '@angular':                           "node_modules/@angular",
+    "@angular/core":                      "node_modules/@angular/core/index.js",
+    "@angular/common":                    "node_modules/@angular/common/index.js",
+    "@angular/compiler":                  "node_modules/@angular/compiler/index.js",
+    "@angular/http":                      "node_modules/@angular/http/index.js",
+    "@angular/platform-browser":          "node_modules/@angular/platform-browser/index.js",
+    "@angular/platform-browser-dynamic":  "node_modules/@angular/platform-browser-dynamic/index.js",
+    "@angular/router-deprecated":         "node_modules/@angular/router-deprecated/index.js",
+    "@angular/router":                    "node_modules/@angular/router/index.js"
   };
 
   var config = {
