ed361ffc4da175aa855fa5b0cd1ba24dfa86ba16
Jay Kan <leicasper@gmail.com>
2016-07-06 10:30:55 -0700

    build(angular2): upgrade to RC4

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,13 +1,13 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { DemoApp } from './demo-app/demo-app';
-import { ROUTER_PROVIDERS } from '@angular/router';
+import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { CUSTOM_EVENTS_PLUGINS } from './plugins/plugins';
 
 bootstrap(DemoApp, [
   ROUTER_PROVIDERS,
   HTTP_PROVIDERS,
-  CUSTOM_EVENTS_PLUGINS,
+  // CUSTOM_EVENTS_PLUGINS, // Todo some issues with the custom dom events plugin
 ])
 .then(success => console.log('Bootstrap success'))
 .catch(error => console.error(error));
