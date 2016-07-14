8fb191e1e6fc280fcc6dc874a97c34f657758647
Jay Kan <leicasper@gmail.com>
2016-07-14 10:32:52 -0700

    feat(core): add generic async PromiseCompleter

---
+++ src/components/core/async/promise-completer.ts
@@ -0,0 +1,13 @@
+// `<R>` is a type variable that identify this `PromiseCompleter` class
+export class PromiseCompleter<R> {
+  promise: Promise<R>;
+  resolve: (value?: R | PromiseLike<R>) => void;
+  reject: (error?: any, stackTrace?: string) => void;
+
+  constructor() {
+    this.promise = new Promise((res, rej) => {
+      this.resolve = res;
+      this.reject = rej;
+    });
+  }
+}
